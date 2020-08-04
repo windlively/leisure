2ce946e8cef3bde474037ff8b51ab2e63e47cc8f
baijh <baijunhan@wecash.net>
2020-08-04 14:01:01 +0800

    add: 日常提交

---
+++ algorithm/src/main/java/lucku/baijunhan/alg/dfs/CourseSchedule.java
@@ -0,0 +1,69 @@
+package lucku.baijunhan.alg.dfs;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 207. 课程表
+ * 你这个学期必须选修 numCourse 门课程，记为 0 到 numCourse-1 。
+ *
+ * 在选修某些课程之前需要一些先修课程。 例如，想要学习课程 0 ，你需要先完成课程 1 ，我们用一个匹配来表示他们：[0,1]
+ *
+ * 给定课程总量以及它们的先决条件，请你判断是否可能完成所有课程的学习？
+ *
+ *
+ *
+ * 示例 1:
+ *
+ * 输入: 2, [[1,0]]
+ * 输出: true
+ * 解释: 总共有 2 门课程。学习课程 1 之前，你需要完成课程 0。所以这是可能的。
+ * 示例 2:
+ *
+ * 输入: 2, [[1,0],[0,1]]
+ * 输出: false
+ * 解释: 总共有 2 门课程。学习课程 1 之前，你需要先完成​课程 0；并且学习课程 0 之前，你还应先完成课程 1。这是不可能的。
+ *
+ *
+ * 提示：
+ *
+ * 输入的先决条件是由 边缘列表 表示的图形，而不是 邻接矩阵 。详情请参见图的表示法。
+ * 你可以假定输入的先决条件中没有重复的边。
+ * 1 <= numCourses <= 10^5
+ */
+public class CourseSchedule {
+
+    class Solution {
+        boolean[] cache;
+
+        public boolean canFinish(int numCourses, int[][] prerequisites) {
+            Map<Integer, List<Integer>> map = new HashMap<>();
+            boolean[] visited = new boolean[numCourses];
+            cache = new boolean[numCourses];
+            for(int[] ns : prerequisites){
+                map.computeIfAbsent(ns[0], k -> new ArrayList<>()).add(ns[1]);
+            }
+
+            for(int i = 0; i < numCourses; i ++){
+                if(!dfs(i, map, visited)) return false;
+            }
+            return true;
+        }
+
+        public boolean dfs(int n, Map<Integer, List<Integer>> map, boolean[] visited){
+            if(cache[n]) return true;
+            if(visited[n]) return false;
+            List<Integer> list = map.get(n);
+            if(list == null || list.isEmpty()) return true;
+            visited[n] = true;
+            for(int i : list){
+                if(!dfs(i, map, visited)) return false;
+            }
+            cache[n] = true;
+            visited[n] = false;
+            return true;
+        }
+    }
+}
