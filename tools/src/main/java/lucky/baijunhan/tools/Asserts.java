43fd20233f272ec94ae6d91f493eca39272062a7
windlively <windlively@live.cn>
2020-12-14 17:03:42 +0800

    add: cron表达式工具

---
+++ tools/src/main/java/lucky/baijunhan/tools/Asserts.java
@@ -0,0 +1,52 @@
+package lucky.baijunhan.tools;
+
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Collection;
+
+public class Asserts {
+
+    public static void notNull(Object obj, String message) {
+        if (obj == null)
+            throw message == null ? new NullPointerException() : new NullPointerException(message);
+    }
+
+    public static void notNull(Object obj) {
+        notNull(obj, null);
+    }
+
+    public static void notEmpty(String str, String message) {
+        if (StringUtils.isEmpty(str))
+            throw message == null ? new IllegalArgumentException() : new IllegalArgumentException(message);
+    }
+
+    public static void notEmpty(String str) {
+        notEmpty(str, null);
+    }
+
+    public static void notEmpty(Collection<?> collection, String message) {
+        if (collection == null || collection.isEmpty())
+            throw message == null ? new IllegalArgumentException() : new IllegalArgumentException(message);
+    }
+
+    public static void notEmpty(Collection<?> collection) {
+        notEmpty(collection, null);
+    }
+
+    public static void isTrue(boolean expression, String message) {
+        if (!expression)
+            throw message == null ? new IllegalStateException() : new IllegalStateException(message);
+    }
+
+    public static void isTrue(boolean expression) {
+        isTrue(expression, null);
+    }
+
+    public static void isFalse(boolean expression, String message) {
+        isTrue(!expression, message);
+    }
+
+    public static void isFalse(boolean expression) {
+        isFalse(expression, null);
+    }
+}
---
+++ tools/src/main/java/lucky/baijunhan/tools/CronExpressionTools.java
@@ -0,0 +1,124 @@
+package lucky.baijunhan.tools;
+
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Arrays;
+import java.util.regex.Pattern;
+
+/**
+ * 文字的时间表达转换为cron表达式
+ */
+public class CronExpressionTools {
+
+    public static final Pattern SQL_PATTERN = Pattern.compile("(?i)^\\s*?SELECT\\s+?(\\*|(\\w+\\s*?,?\\s*?))+?\\s+FROM.+", Pattern.CASE_INSENSITIVE);
+
+    // 每分钟触发时间
+    public static final Pattern TRIGGER_CONFIG_FOR_MINUTELY_PATTERN = Pattern.compile("[12345]?\\d秒");
+
+    // 每小时触发时间
+    public static final Pattern TRIGGER_CONFIG_FOR_HOURLY_PATTERN = Pattern.compile("[12345]?\\d分" + TRIGGER_CONFIG_FOR_MINUTELY_PATTERN + "");
+
+    // 每日触发时间
+    public static final Pattern TRIGGER_CONFIG_FOR_DAILY_PATTERN = Pattern.compile("[12]?\\d时" + TRIGGER_CONFIG_FOR_HOURLY_PATTERN + "");
+
+    // 每月触发时间
+    public static final Pattern TRIGGER_CONFIG_FOR_MONTHLY_PATTERN = Pattern.compile("(([123]?\\d日)|(月末))" + TRIGGER_CONFIG_FOR_DAILY_PATTERN + "");
+
+    // 每年触发配置
+    public static final Pattern TRIGGER_CONFIG_FOR_YEARLY_PATTERN = Pattern.compile("1?\\d月" + TRIGGER_CONFIG_FOR_MONTHLY_PATTERN + "");
+
+    // 每周触发配置
+    public static final Pattern TRIGGER_CONFIG_FOR_WEEKLY_PATTERN = Pattern.compile("周[一二三四五六日]" + TRIGGER_CONFIG_FOR_DAILY_PATTERN);
+
+    public static String triggerConfigToCronExpression(String triggerConfig) {
+        Asserts.isTrue(StringUtils.isNotEmpty(triggerConfig), "trigger config is empty");
+        String[] split = triggerConfig.split("/");
+        String first = split[0];
+        String time = split[1];
+        String s = "";
+        switch (first) {
+            case "每年": {
+                s = "$4 $3 $2 $1 $0 ? *";
+                Asserts.isTrue(time.matches("^" + TRIGGER_CONFIG_FOR_YEARLY_PATTERN.pattern() + "$"),
+                        "trigger config " + time + ", not match pattern: " + TRIGGER_CONFIG_FOR_YEARLY_PATTERN);
+
+                if(time.contains("月末")){
+                    s = "$3 $2 $1 L $0 ? *";
+                    time = time.replace("月末","");
+                }
+
+                s = setCronParam(time, s);
+                break;
+            }
+            case "每月": {
+                s = "$3 $2 $1 $0 * ? *";
+                Asserts.isTrue(time.matches("^" + TRIGGER_CONFIG_FOR_MONTHLY_PATTERN.pattern() + "$"),
+                        "trigger config " + time + ", not match pattern: " + TRIGGER_CONFIG_FOR_MONTHLY_PATTERN);
+
+                if(time.startsWith("月末")){
+                    s = "$2 $1 $0 L * ? *";
+                    time = time.substring(2);
+                }
+
+                s = setCronParam(time, s);
+                break;
+            }
+            case "每周": {
+                s = "$2 $1 $0 ? * $week *";
+                Asserts.isTrue(time.matches("^" + TRIGGER_CONFIG_FOR_WEEKLY_PATTERN.pattern() + "$"),
+                        "trigger config " + time + ", not match pattern: " + TRIGGER_CONFIG_FOR_MONTHLY_PATTERN);
+                s = s.replace("$week", "" + getWeakFromChinese(time.substring(0, 2)));
+                time = time.substring(2);
+                s = setCronParam(time, s);
+                break;
+            }
+            case "每日": {
+                s = "$2 $1 $0 * * ? *";
+                Asserts.isTrue(time.matches("^" + TRIGGER_CONFIG_FOR_DAILY_PATTERN.pattern() + "$"),
+                        "trigger config " + time + ", not match pattern: " + TRIGGER_CONFIG_FOR_DAILY_PATTERN);
+                s = setCronParam(time, s);
+                break;
+            }
+            case "每时": {
+                s = "$1 $0 * * * ? *";
+                Asserts.isTrue(time.matches("^" + TRIGGER_CONFIG_FOR_HOURLY_PATTERN.pattern() + "$"),
+                        "trigger config " + time + ", not match pattern: " + TRIGGER_CONFIG_FOR_HOURLY_PATTERN);
+                s = setCronParam(time, s);
+                break;
+            }
+            case "每分": {
+                s = "$0 * * * * ? *";
+                Asserts.isTrue(time.matches("^" + TRIGGER_CONFIG_FOR_MINUTELY_PATTERN.pattern() + "$"),
+                        "trigger config " + time + ", not match pattern: " + TRIGGER_CONFIG_FOR_MINUTELY_PATTERN);
+                s = setCronParam(time, s);
+                break;
+            }
+            case "每秒":
+                s = "* * * * * ? *";
+                break;
+            default:
+                throw new IllegalStateException("unknown scheduled time config: " + first);
+        }
+        return s;
+    }
+
+    private static int getWeakFromChinese(String word) {
+        return Arrays.asList("周日", "周一", "周二", "周三", "周四", "周五", "周六").indexOf(word) + 1;
+    }
+
+    private static String setCronParam(String time, String s) {
+        String[] strings = time.split("[月日时分秒]");
+        for (int i = 0, stringsLength = strings.length; i < stringsLength; i++) {
+            String s1 = strings[i];
+            String unit = "$" + i;
+            s = s.replace(unit, s1);
+        }
+        return s;
+    }
+
+
+    public static void main(String[] args) {
+        String s = "每年/2月月末22时9分10秒";
+        System.out.println(triggerConfigToCronExpression(s));
+    }
+}
